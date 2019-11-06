cde7926daf22d21c9873e290c6404fcf9c3cf4b5
Paiste1 <alexpaiste.ap@gmail.com>
2019-11-07 01:41:11 +0800

    вп перекинут, начало создания сайта

--- wp-config.php
+++ wp-config.php
@@ -32,7 +32,7 @@ define( 'DB_PASSWORD', 'tfRUAYjd0LK8YaGD' );
 define( 'DB_HOST', 'localhost' );
 
 /** Кодировка базы данных для создания таблиц. */
-define( 'DB_CHARSET', 'utf8' );
+define( 'DB_CHARSET', 'utf8mb4' );
 
 /** Схема сопоставления. Не меняйте, если не уверены. */
 define( 'DB_COLLATE', '' );
@@ -46,14 +46,14 @@ define( 'DB_COLLATE', '' );
  *
  * @since 2.6.0
  */
-define( 'AUTH_KEY',         'впишите сюда уникальную фразу' );
-define( 'SECURE_AUTH_KEY',  'впишите сюда уникальную фразу' );
-define( 'LOGGED_IN_KEY',    'впишите сюда уникальную фразу' );
-define( 'NONCE_KEY',        'впишите сюда уникальную фразу' );
-define( 'AUTH_SALT',        'впишите сюда уникальную фразу' );
-define( 'SECURE_AUTH_SALT', 'впишите сюда уникальную фразу' );
-define( 'LOGGED_IN_SALT',   'впишите сюда уникальную фразу' );
-define( 'NONCE_SALT',       'впишите сюда уникальную фразу' );
+define( 'AUTH_KEY',         '2It[HoRV3isZ90&A4AC]& qBj/`}<gUOgo8]i._X>6%i(]|s&;&E`?&9CZMhKJ#0' );
+define( 'SECURE_AUTH_KEY',  'E~zc#ad=nmGO=$Pv8FVSp[W{H[LTv?9gs3w,~F=@O2/%r:^~L+U$nwozkOK{Qv1!' );
+define( 'LOGGED_IN_KEY',    'jcQsi/BeLld.rP<_h)?v/ JvPIuq-)=)~t|i*;HXUhs(/QYX{XnuKx&o4(dE~6h1' );
+define( 'NONCE_KEY',        'JW~ZVAg:Grw{%r9x?G`2v^S7of9clS)A<?J4Ep5MAm,w.Ni-fKOly+U(^*I~A^p4' );
+define( 'AUTH_SALT',        '9r_E*zLat(/-[>-C}kDbOr[])LY|p0TOSek8hkNgoJJBM`R7n,Yy3TWS?d)[7CJM' );
+define( 'SECURE_AUTH_SALT', 'JLrbV=wMM? WQq]c&P=R)b8^}VFE_VYa;:Q^Bves39&P@#@ 8w7{n{B}u0toK$x>' );
+define( 'LOGGED_IN_SALT',   'R-5}AlQpA/@`c(i=$>?=:|&+l`HCV^G76#)k`VQTz?CCDwEM};p&P:S9a[I7y,B[' );
+define( 'NONCE_SALT',       'LQt,`P3nepY.CO(&^.E;g*$p3!J13mc|S2_HhHq;?T_>w+mui2toz=B+.b-+i7oj' );
 
 /**#@-*/
 
